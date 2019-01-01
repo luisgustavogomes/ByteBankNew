2ca70bd1caceaf3cb5cde173de0192dac9fc8d14
luisgustavogomes <luisgustavogomes@outlook.com>
2019-01-01 14:23:09 -0200

    Utilizando NuGet

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -12,11 +13,40 @@ namespace ByteBank.SistemaAgencia
     {
         public static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(847, 847);
-            new ContaCorrente(874, 876);
-            Console.WriteLine("Número: " + conta.Numero + "\nAgencia: " + conta.Agencia);
+            DateTime dataFimPagamento = new DateTime(2019,1,04);
+            DateTime dataAtual = DateTime.Now;
+            //TimeSpan diff = dataFimPagamento - dataAtual;
+            TimeSpan diff = TimeSpan.FromMinutes(60*24);
+
+            Console.WriteLine("Data fim pagamento: " + dataFimPagamento);
+            Console.WriteLine("Data atual: " + dataAtual);
+            Console.WriteLine("Diferença de datas: " + TimeSpanHumanizeExtensions.Humanize(diff) );
+
 
             Console.ReadLine();
         }
+
+        //public static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
+        //{
+        //    if (timeSpan.Days > 30 )
+        //    {
+        //        int quantidadeMeses = timeSpan.Days / 30;
+        //        if (quantidadeMeses == 1 )
+        //        {
+        //            return quantidadeMeses + " mês";
+        //        }
+        //        return quantidadeMeses + " meses";
+        //    }
+        //    else if (timeSpan.Days > 7)
+        //    {
+        //        int quantidadeSemanas = timeSpan.Days / 7;
+        //        if (quantidadeSemanas == 1)
+        //        {
+        //            return quantidadeSemanas + " semana";
+        //        }
+        //        return quantidadeSemanas + " semanas";
+        //    }
+        //    return timeSpan.Days + " Dias";
+        //}
     }
 }
